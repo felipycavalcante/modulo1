53d6f2800b021680bb86c5c4d7b48062579c33ae
Felipy Cavalcante <32340109+felipycavalcante@users.noreply.github.com>
2019-03-20 21:26:48 -0300

    Update m1_exercicio1.py

--- exercicios/code/m1_exercicio1.py
+++ exercicios/code/m1_exercicio1.py
@@ -1,29 +1,15 @@
-'''
-classe: m1_lab1.py
-descricao: Listar os 3 primeiros caracteres do nome do vereador, pois em alguns casos o número disponível de caracteres é
-apenas 3, e não 8, como estava anteriormente.
-autor: Clodonil Honorio Trigo
-email: clodonil@nisled.org
-data de entrega: 13/03/2019
-'''
-
-# importa a lib para obter as tabelas da Wikipedia
 from  lib.scrapy_table import Scrapy_Table
-
 # Variavel com o link da tabela
-url="https://pt.wikipedia.org/wiki/C%C3%A2mara_Municipal_de_S%C3%A3o_Paulo"
-
-# Inicia a class para obter a tabela
+url="https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_do_Brasil_por_popula%C3%A7%C3%A3o"
 site_connect = Scrapy_Table(url)
-
-# Pegando a tabela 5 (Vereadores em exercicio)
-tables = site_connect.get_tables(5)
+tables = site_connect.get_tables(0)
   
 # Listando o conteudo da tabela
 for linha in tables[1:]:
 
-     # Obtendo o nome. Esta na primeira posicao da lista
-     nome = linha[0]
+     # Obtendo número e nome
+     nome = linha[1]
+     nome2 = linha[2]
 
-     # Imprimindo os caracteres da posicao 0 ate 2 (0, 1, 2 e 3) 
-     print(nome[0:3])
+     # Imprimindo os caracteres
+     print(nome[0:15], nome2[0:35])
